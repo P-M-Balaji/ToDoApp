006cc0c462b7952e99cd473512ba22647b8b18ba
Balaji PM <45323249+P-M-Balaji@users.noreply.github.com>
2021-09-17 12:10:51 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm, UpdateForm
+
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    tasks = Task.objects.all()
+    form = TaskForm()
+    print(form)
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
+
+
+def edit(request, id=id):
+    task = Task.objects.get(id=id)
+    if request.method == 'POST':
+        form = UpdateForm(request.POST or None, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home:home')
+
+    form = UpdateForm(request.POST or None, instance=task)
+    print(form)
+    return render(request, 'edit.html', {'form': form})
+
+
+def completed(request, id):
+    task = Task.objects.get(id=id)
+    if not task.completed:
+        task.completed = True
+        task.save()
+        return redirect('home:home')
+
+
+def delete(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('home:home')
+
+
+def filter_priority(request, choice):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    tasks = Task.objects.filter(priority=choice)
+    form = TaskForm()
+    print(form)
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
+
+
+def filter_status(request, choice):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    tasks = Task.objects.filter(completed=choice)
+    form = TaskForm()
+    print(form)
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
